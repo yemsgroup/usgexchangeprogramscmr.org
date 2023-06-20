5899ad613002889ee8685cf6d17817139f63522d
Yembe Nfor <nfor@yems.group>
2023-06-20 15:31:57 +0100

    Design: Updated Updates and Homepage Designs (removed application prompts, added news to homepage)

--- content/themes/bootstrap_child/home.php
+++ content/themes/bootstrap_child/home.php
@@ -7,7 +7,7 @@
     ?>
 
 
-    <div class="container-fluid bg-custom-primary text-white">
+    <div class="d-none container-fluid bg-custom-primary text-white">
         <div class="container py-5 text-center lead">
             <div class="row justify-content-center">
                 <div class="col-11 text-white">
@@ -42,7 +42,7 @@
     </div>
 
 
-    <div class="container-fluid">
+    <div class="d-none container-fluid">
         <div class="row">
             <div class="col-12 px-0 text-center">
                 <a href="https://questionpro.com/t/AWcJ6Zwnta" target="_blank" class="btn btn-lg d-block btn-custom-accent-green py-5 text-uppercase fw-bold">Apply to attend the upcoming Symposium</a>
@@ -54,9 +54,12 @@
     <div class="container my-5 py-5">
         <div class="row row-cols-lg-3 justify-content-center">
 
-            <?php // Feature::list_features('News', $params = ['limit' => 3]); ?>
+            <div class="col-12">
+                <h3 class="mb-4 text-center text-custom-secondary text-uppercase">From our News Center</h3>
+            </div>
+            <?php Feature::list_features('News', $params = ['limit' => 3]); ?>
 
-            <div class="col-lg-8 text-center">
+            <div class="mt-5 col-lg-8 text-center">
                 <h3 class="mb-4 text-custom-secondary text-uppercase">Our Partners & Sponsors</h3>
                 <?php Feature::list_features('Partner', null, 'partners-preview.html'); ?>
             </div>
